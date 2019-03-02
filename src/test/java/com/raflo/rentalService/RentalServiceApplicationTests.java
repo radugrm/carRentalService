a195e8a1b9d029df1debf83c8c27b394f949d23f
radugr <radugrecu_rg@yahoo.com>
2019-03-02 10:07:54 +0200

    first commit

---
+++ src/test/java/com/raflo/rentalService/RentalServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.raflo.rentalService;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RentalServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
