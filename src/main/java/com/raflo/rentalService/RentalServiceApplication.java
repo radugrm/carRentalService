fb79feef043fede5889d6c1fd63906f852535752
radugr <radu.grecu.rg@gmail.com>
2019-03-08 09:38:11 +0200

    modified main class

--- src/main/java/com/raflo/rentalService/RentalServiceApplication.java
+++ src/main/java/com/raflo/rentalService/RentalServiceApplication.java
@@ -2,11 +2,18 @@ package com.raflo.rentalService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class RentalServiceApplication {
+public class RentalServiceApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(RentalServiceApplication.class);
+	}
+
+	public static void main(String[] args)  {
 		SpringApplication.run(RentalServiceApplication.class, args);
 	}
 
